6d5a8f04bb51b2dd0389d209b2aa77cd29670ecd
igormivanov <igormivanovx@gmail.com>
2022-12-24 13:02:18 -0300

    Alterando e Excluindo dados

--- src/main/java/com/estudo/springweb/controllers/AdministradoresController.java
+++ src/main/java/com/estudo/springweb/controllers/AdministradoresController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.estudo.springweb.models.Administrador;
 import com.estudo.springweb.repository.AdministradoresRepository;
@@ -21,5 +23,22 @@ public class AdministradoresController {
 		List<Administrador> administradores = (List<Administrador>) administradoresRepository.findAll();
 		model.addAttribute("administradores", administradores);
 		return "administradores/index";
+	}
+	
+	@GetMapping("/administradores/novo")
+	public String novo() {
+		return "administradores/novo";
+	}
+	
+	@PostMapping("/administradores/criar")
+	public String criar(Administrador administrador) {
+		administradoresRepository.save(administrador);
+		return "redirect:/administradores";
+	}
+	
+	@GetMapping("/administradores/{id}/excluir")
+	public String excluir(@PathVariable int id) {
+		administradoresRepository.deleteById(id);
+		return "redirect:/administradores";
 	}
 }
